a1c2e3d92b67ab501cdf51f598989dda21910a47
Priyanshu <priyanshupanditlg12a@gmail.com>
2023-03-29 22:15:49 +0530

    initial html structure ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Highlights from './components/Highlights';
+import Testimonials from './components/Testimonials';
+import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      Home Page
-    </div>
+    <>
+      <Nav></Nav>
+      <Main></Main>
+      <Highlights></Highlights>
+      <Testimonials></Testimonials>
+      <About></About>
+      <Footer></Footer>
+    </>
   );
 }
 
---
+++ src/components/Highlights.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Highlights(){
+    return(
+        <section>
+            <section>
+                <h1>Specials</h1>
+                <button>Create Menu</button>
+            </section>
+            <section>
+                <article>
+                    <img src="" alt="dish1" />
+                    <div>
+                        <h4>Name1</h4>
+                        <h4>prize</h4>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor earum eum velit quasi, suscipit nulla molestias esse maxime, animi rem eius consectetur ut, deserunt recusandae aliquam laudantium repudiandae quibusdam asperiores?</p>
+                    <p>Order a delivary</p>
+                </article>
+                <article>
+                    <img src="" alt="dish2" />
+                    <div>
+                        <h4>Name2</h4>
+                        <h4>prize</h4>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor earum eum velit quasi, suscipit nulla molestias esse maxime, animi rem eius consectetur ut, deserunt recusandae aliquam laudantium repudiandae quibusdam asperiores?</p>
+                    <p>Order a delivary</p>
+                </article>
+                <article>
+                    <img src="" alt="dish3" />
+                    <div>
+                        <h4>Name3</h4>
+                        <h4>prize</h4>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor earum eum velit quasi, suscipit nulla molestias esse maxime, animi rem eius consectetur ut, deserunt recusandae aliquam laudantium repudiandae quibusdam asperiores?</p>
+                    <p>Order a delivary</p>
+                </article>
+            </section>
+        </section>
+    );
+}
+
+export default Highlights;
